5cbfe9e19cbd3a723261e83ebec72c12e7619c92
blocsu <dotsenko.sar@gmail.com>
2024-12-31 16:14:35 +0400

    Environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -6,8 +7,14 @@ import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
 import { TopPageModule } from './top-page/top-page.module';
 
+
 @Module({
-  imports: [AuthModule, TopPageModule, ProductModule, ReviewModule],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    TopPageModule,
+    ProductModule,
+    ReviewModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -1,9 +1,11 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { FindTopPageDto } from './dto/find-top-page.dto';
 import { TopPageModel } from './top-page.model/top-page.model';
 
 @Controller('top-page')
 export class TopPageController {
+	constructor(private readonly configService: ConfigService) {}
 	@Post('create')
 	async create(@Body() dto: Omit<TopPageModel, '_id'>) {
 
@@ -11,7 +13,7 @@ export class TopPageController {
 
 	@Get(':id')
 	async get(@Param('id') id: string) {
-
+		this.configService.get('TEST');
 	}
 
 	@Delete(':id')
