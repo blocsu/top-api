fea3cbe4b9d8537068f70efb986657e1d8467561
blocsu <dotsenko.sar@gmail.com>
2025-01-08 14:15:41 +0400

    Authorization and generation JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { TypegooseModule } from 'nestjs-typegoose';
+import { getJwtConfig } from 'src/configs/jwt.config';
 import { AuthController } from './auth.controller';
-import { UserModel } from './user.model';
 import { AuthService } from './auth.service';
+import { UserModel } from './user.model';
 
 @Module({
 	controllers: [AuthController],
@@ -14,7 +17,12 @@ import { AuthService } from './auth.service';
 					collection: 'User'
 				}
 			}
-		])
+		]),
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getJwtConfig
+		})
 	],
 	providers: [AuthService]
 })
